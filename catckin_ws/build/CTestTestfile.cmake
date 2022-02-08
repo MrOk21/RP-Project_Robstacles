9e289a5cf4c253397ed669b91d3a6be8ff27621a
Vincenzo Vitale <vitale.1697989@studenti.uniroma1.it>
2022-02-08 12:36:11 +0100

    Creation of the ROS workspace and the package related to the project.

---
+++ catckin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mrok21/RP-Project_Robstacles/catckin_ws/src
+# Build directory: /home/mrok21/RP-Project_Robstacles/catckin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robstacles")
---
+++ catckin_ws/build/robstacles/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mrok21/RP-Project_Robstacles/catckin_ws/src/robstacles
+# Build directory: /home/mrok21/RP-Project_Robstacles/catckin_ws/build/robstacles
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
